64a3a8369e3254b8f44599e5fab8d586a4906411
Sarthak <omersarthak11@gmail.com>
2023-01-09 01:30:06 +0530

    added home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from "./pages/navbar"
 import Footer from "./pages/footer"
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <Footer/>
     </div>
   );
--- frontend/src/pages/navbar.js
+++ frontend/src/pages/navbar.js
@@ -1,87 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "../images/logo.png";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import Sidebar from "../components/Sidebar";
 function Navbar() {
-  const [nav, setNav] = useState(false);
-  const handleNav = () => {
-    setNav(!nav);
-  };
+  const EventLinks = [
+    { Event: "Home", Link: "/" },
+    { Event: "ABOUT", Link: "/" },
+    { Event: "EVENTS", Link: "/" },
+    { Event: "SPONSORS", Link: "/" },
+    { Event: "ORGANIZERS", Link: "/" },
+    { Event: "GALLERY", Link: "/" },
+  ];
   return (
     <>
-      <div className="flex justify-between items-center bg-transparent">
+      <div className="flex justify-between items-center bg-transparent fixed top-0 w-full z-10">
         <div>
           <img src={logo} alt="loading" className="h-14 p-1.5 md:h-20 md:p-2" />
         </div>
-        <div className="mr-10">
+        <div className="mr-10 text-white">
           <ul className="hidden md:flex">
-            <a href="/" className="">
-              {" "}
-              <li className="p-4">HOME</li>
-            </a>
-            <a href="/" className="">
-              {" "}
-              <li className="p-4">ABOUT</li>
-            </a>
-            <a href="/" className="">
-              {" "}
-              <li className="p-4">EVENTS</li>
-            </a>
-            <a href="/" className="">
-              {" "}
-              <li className="p-4">SPONSORS</li>
-            </a>
-            <a href="/" className="">
-              {" "}
-              <li className="p-4">ORGANIZERS</li>
-            </a>
-            <a href="/" className="">
-              {" "}
-              <li className="p-4">GALLERY</li>
-            </a>
-            <button className="p-2 rounded m-2 uppercase hover:bg-red-500 hover:text-white transition duration-500">registration</button>
+            {EventLinks.map(({ Event, Link }) => (
+              <a href={Link}>
+                <li className="p-4">{Event}</li>
+              </a>
+            ))}
+            <button className="p-2 rounded m-2 uppercase hover:bg-red-500 hover:text-white transition duration-500">
+              registration
+            </button>
           </ul>
         </div>
-        <div className="block md:hidden" onClick={handleNav}>
-          {nav ? (
-            <AiOutlineClose size={30} className="m-3" />
-          ) : (
-            <AiOutlineMenu size={30} className="m-3" />
-          )}
-        </div>
-        <div
-          className={
-            nav
-              ? "fixed left-0 top-0 w=[60%] h-full border-r bg-black text-white border-r-white-900 ease-in-out duration-500"
-              : "fixed left-[-100%]"
-          }
-        >
-          <ul className="pt-24">
-            <a href="/">
-              {" "}
-              <li className="p-4 border-b border-gray-600">HOME</li>{" "}
-            </a>
-            <a href="/">
-              {" "}
-              <li className="p-4 border-b border-gray-600">ABOUT</li>{" "}
-            </a>
-            <a href="/">
-              {" "}
-              <li className="p-4 border-b border-gray-600">EVENTS</li>{" "}
-            </a>
-            <a href="/">
-              {" "}
-              <li className="p-4 border-b border-gray-600">SPONSORS</li>{" "}
-            </a>
-            <a href="/">
-              {" "}
-              <li className="p-4 border-b border-gray-600">ORGANIZERS</li>{" "}
-            </a>
-            <a href="/">
-              {" "}
-              <li className="p-4 border-b border-gray-600">GALLERY</li>{" "}
-            </a>
-          </ul>
-        </div>
+        <Sidebar />
       </div>
     </>
   );
